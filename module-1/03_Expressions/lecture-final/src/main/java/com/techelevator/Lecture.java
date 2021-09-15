e8fbba4f128c8d98794f42ba51feecb8c6bcf2eb
Brian Lauvray <brian@techelevator.com>
2021-09-15 14:32:00 -0400

    Added lecture code

--- module-1/03_Expressions/lecture-final/src/main/java/com/techelevator/Lecture.java
+++ module-1/03_Expressions/lecture-final/src/main/java/com/techelevator/Lecture.java
@@ -6,7 +6,7 @@ public class Lecture {
     it so that it returns something other than a 1.
     */
     public int returnNotOne() {
-        return 1;
+        return 2;
     }
 
     /*
@@ -14,29 +14,29 @@ public class Lecture {
     it so that it returns something other than a 0.5.
     */
     public double returnNotHalf() {
-        return 0.5;
+        return 1;
     }
 
     /*
     3. This method needs to return a String. Fix it to return a valid String.
     */
     public String returnName() {
-        return null;
+        return "Java";
     }
 
     /*
     4. This method currently returns an int. Change it so that it returns a double.
     */
-    public int returnDoubleOfTwo() {
-        return 2;
+    public double returnDoubleOfTwo() {
+        return (double) 2;
     }
 
     /*
     5. This method should return the language that you're learning. Change
     it so that it does that.
     */
-    public boolean returnNameOfLanguage() {
-        return false;
+    public String returnNameOfLanguage() {
+        return "Java";
     }
 
     /*
@@ -45,10 +45,10 @@ public class Lecture {
     */
     public boolean returnTrueFromIf() {
         if (true) {
-            return false;
+            return true;
         }
 
-        return false;
+       return false;
     }
 
     /*
@@ -69,9 +69,7 @@ public class Lecture {
     */
     public boolean returnTrueWhenGreaterThanFive(int number) {
         if (number > 5) {
-
-        } else {
-
+            return true;
         }
         return false;
     }
@@ -92,13 +90,13 @@ public class Lecture {
     */
     public int returnNumberAfterAddThreeAndAddFive(int number, boolean addThree, boolean addFive) {
         if (addThree) {
-            number = number + 1;
+            number = number + 3;
         }
 
         // We can't use an else here. They could both be true, so we have to check each one.
 
         if (addFive) {
-            number += 1;
+            number += 5;
         }
 
         return number;
@@ -108,14 +106,24 @@ public class Lecture {
     11. Write an if statement that returns "Fizz" if the parameter is 3 and returns an empty String for anything else.
     */
     public String returnFizzIfThree(int number) {
-        return "";
+        // number is 3 then return "Fizz"
+        // else return ""
+        String fizzString = "";
+        if (number == 3) {
+            fizzString = "Fizz";
+        } else {
+            fizzString = "";
+        }
+        return fizzString;
     }
 
     /*
     12. Now write the above using the Ternary operator ?:. If you're not sure what this is, you can Google it.
     */
     public String returnFizzIfThreeUsingTernary(int number) {
-        return "";
+        String fizzString = number == 3 ? "Fizz" : "";
+
+        return fizzString;
     }
 
     /*
@@ -158,6 +166,28 @@ public class Lecture {
         } else {
             return "Minor";
         }
+    }
+
+
+    public static void main(String[] args) {
+
+        boolean shouldRunCode = false;
+        int numX = 7;
+        int numY = 0;
+
+        if (numX % 7 == 0 && numX % 10 == 0) {
+            System.out.println("the value was divisible by both");
+        } else if ( numX % 7 == 0) {
+            System.out.println("the value was divisible by 7");
+        } else if (numX % 10 == 0) {
+            System.out.println("the value was divisible by 10");
+        } else {
+            System.out.println("the value was not divisible by 7 or 10");
+        }
+
+        System.out.println("This always runs");
+
+
     }
 
 }
