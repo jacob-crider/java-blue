78dbbece9646f7c296b6240389dfcee39e7b0fe2
Brian Lauvray <brian@techelevator.com>
2021-09-16 14:24:47 -0400

    Lecture code

--- module-1/04_Loops_Arrays/lecture-final/src/main/java/com/techelevator/ArraysAndLoops.java
+++ module-1/04_Loops_Arrays/lecture-final/src/main/java/com/techelevator/ArraysAndLoops.java
@@ -29,6 +29,76 @@ public class ArraysAndLoops {
         System.out.println("funky address ref " + count);
         System.out.println("I want to see the first " + count[0]);
         System.out.println(instructors[1] + " " + instructors[4]);
+
+        for( int i = 0 ; i < 10; i++)
+        {
+            System.out.println(i + " ");
+        }
+
+        int sum = 0;
+
+        for ( int i = 0; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+
+        for (int i = 100; i > 0; i--) {
+            System.out.println(i);
+        }
+
+        // Continue stops the current execution of the block and goes to the next
+        // increment in the loop
+        for ( int counter = 0; counter < 100; counter++) {
+            if ( counter % 10 == 0) {
+                continue;
+            }
+            System.out.println(counter);
+        }
+
+        // Break stops the current execution of the block and immediately ends the loop
+        for ( int counter = 0; counter < 100; counter++) {
+            if ( counter % 10 == 0) {
+                break;
+            }
+            System.out.println(counter);
+        }
+
+        for (int i = 0; i < instructors.length; i++) {
+            System.out.println( instructors[i] );
+        }
+
+        for (int i = 0; i < instructors.length; i++) {
+            if ( i % 2 == 0) {
+                System.out.println( instructors[i] );
+            }
+        }
+        //int[] count = new int[] {10, 20, 30, 40, 50};
+        for (int i = 0; i < count.length; i++) {
+            count[i] = count[i] * 2;
+        }
+
+        // When 200 is found, skip it and the next number
+        for (int i = 0; i < count.length; i++) {
+
+            if (count[i] == 200) {
+                i++;
+                continue;
+            }
+            System.out.println(count[i]);
+        }
+
+        // When 200 is found, print it and the next number
+        for (int i = 0; i < count.length; i++) {
+
+            if (count[i] == 100) {
+                System.out.println(count[i]);
+                if (i  < count.length - 1) {
+                    System.out.println(count[i + 1]);
+                }
+            }
+
+        }
+
     }
 
 }
