10e539dc68fb7eeed123f0b9a7965b74316bd742
Jacob Crider <jpc214@gmail.com>
2021-09-18 14:59:24 -0400

    partial submit for Fibonacci exercise

--- module-1/05_Command_Line_Programs/exercise-student/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05_Command_Line_Programs/exercise-student/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,29 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
-	public static void main(String[] args) {
+	public static int main(String[] args) {
+
+		Scanner userInput = new Scanner(System.in);
+
+
+		System.out.print("Please enter a number: ");
+		int numberFromUser;
+
+	public static int fibCalc(int i, n) {
+
+		int integerOne = 0;
+		int integerTwo = 1;
+
+		for (int i = 1; i <= numberFromUser; i++) {
+			if (numberFromUser <= 0) {
+				System.out.println("0, 1");;
+
+			}
+		}
+
 
 	}
 
