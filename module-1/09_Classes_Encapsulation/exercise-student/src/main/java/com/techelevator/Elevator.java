5b63c82772fe685ff57e9f52818f408732528bad
Jacob Crider <jpc214@gmail.com>
2021-09-23 19:11:27 -0400

    partial commit for Elevator exercise M1D9

--- module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/Elevator.java
+++ module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/Elevator.java
@@ -2,4 +2,61 @@ package com.techelevator;
 
 public class Elevator {
 
+    // Instance Variables
+    private int currentFloor = 1;
+    private int numberOfFloors;
+    private boolean doorOpen;
+
+    // Constructor
+    public Elevator(int numberOfLevels) {
+        this.numberOfFloors = numberOfLevels;
+    }
+
+    // Methods
+    public boolean isDoorOpen() {
+        return false;
+    }
+
+    public void openDoor() {
+        doorOpen = true;
+    }
+
+    public void closeDoor() {
+        doorOpen = false;
+    }
+
+    public void goUp(int desiredFloor) {
+        if (isDoorOpen()) {
+            if (desiredFloor >= 1) {
+                if (desiredFloor > currentFloor) {
+                    currentFloor = desiredFloor;
+                }
+            }
+        }
+    }
+
+    public void goDown(int desiredFloor) {
+        if (isDoorOpen()) {
+            if (desiredFloor < numberOfFloors) {
+                if (desiredFloor > currentFloor) {
+                    currentFloor = desiredFloor;
+                }
+            }
+        }
+
+    }
+
+// Getters
+    public int getCurrentFloor() {
+       return currentFloor;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public boolean getDoorOpen() {
+        return doorOpen;
+    }
+
 }
