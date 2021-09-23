6a7d718696aa297eee179cf3633b6e6e393efe92
Jacob Crider <jpc214@gmail.com>
2021-09-23 18:27:55 -0400

    submitting Airplane exercise M1D9

--- module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/Airplane.java
+++ module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/Airplane.java
@@ -2,6 +2,67 @@ package com.techelevator;
 
 public class Airplane {
 
+// Instance Variables
+    public String planeNumber;
+    public int totalFirstClassSeats;
+    public int bookedFirstClassSeats;
+    public int totalCoachSeats;
+    public int bookedCoachSeats;
 
+// Constructor
+    public Airplane(String planeNumber, int totalFirstClassSeats, int totalCoachSeats){
+        this.planeNumber = planeNumber;
+        this.totalFirstClassSeats = totalFirstClassSeats;
+        this.totalCoachSeats = totalCoachSeats;
+    }
+
+// Methods
+    public boolean reserveSeats(boolean forFirstClass, int totalNumberOfSeats) {
+        if (forFirstClass == true) {
+            if (totalFirstClassSeats - bookedFirstClassSeats >= totalNumberOfSeats) {
+                bookedFirstClassSeats += totalNumberOfSeats;
+                return true;
+            } else {
+                return false;
+            }
+        }
+        if (forFirstClass == false && totalCoachSeats - bookedCoachSeats >= totalNumberOfSeats) {
+                bookedCoachSeats += totalNumberOfSeats;
+                return true;
+            }
+            return false;
+        }
+
+
+
+// Getters
+    public String getPlaneNumber() {
+        return planeNumber;
+    }
+
+    public int getTotalFirstClassSeats() {
+        return totalFirstClassSeats;
+    }
+
+    public int getBookedFirstClassSeats() {
+        return bookedFirstClassSeats;
+    }
+
+    public int getTotalCoachSeats() {
+        return totalCoachSeats;
+    }
+
+    public int getBookedCoachSeats() {
+        return bookedCoachSeats;
+    }
+
+// Derived Getters
+    public int getAvailableFirstClassSeats() {
+        return totalFirstClassSeats - bookedFirstClassSeats;
+    }
+
+    public int getAvailableCoachSeats() {
+        return totalCoachSeats - bookedCoachSeats;
+    }
 
 }
