5eb74b684783b7037439467875aa12b5f3a5ec7d
Jacob Crider <jpc214@gmail.com>
2021-09-23 16:06:23 -0400

    submitting FruitTree exercise M1D9

--- module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/FruitTree.java
+++ module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/FruitTree.java
@@ -2,8 +2,32 @@ package com.techelevator;
 
 public class FruitTree {
 
-    public boolean pickFruit(int numberOfPiecesOfFruit) {
-        return false;
+    private String typeOfFruit;
+    private int piecesOfFruitLeft;
+
+
+    public FruitTree(String typeOfFruit, int startingPiecesOfFruit) {
+        this.piecesOfFruitLeft = startingPiecesOfFruit;
+        this.typeOfFruit = typeOfFruit;
+    }
+
+    public String getTypeOfFruit() {
+        return typeOfFruit;
+    }
+
+    public int getPiecesOfFruitLeft() {
+        return piecesOfFruitLeft;
+    }
+
+    public boolean pickFruit(int numberOfPiecesToRemove){
+        if (piecesOfFruitLeft >= numberOfPiecesToRemove) {
+            piecesOfFruitLeft -= numberOfPiecesToRemove;
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
+
+
