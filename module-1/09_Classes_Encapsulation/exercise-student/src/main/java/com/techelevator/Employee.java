6244c6d1ad83d3de18c70cab9fb396f3930ad812
Jacob Crider <jpc214@gmail.com>
2021-09-23 17:46:38 -0400

    submitting Employee exercise M1D9

--- module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/Employee.java
+++ module-1/09_Classes_Encapsulation/exercise-student/src/main/java/com/techelevator/Employee.java
@@ -2,4 +2,59 @@ package com.techelevator;
 
 public class Employee {
 
+    // Instance Variables
+    private int employeeId;
+    private String firstName;
+    private String lastName;
+    private String department;
+    private double annualSalary;
+
+    // Constructor
+    public Employee(int employeeId, String firstName, String lastName, double salary) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.annualSalary = salary;
+    }
+    // Methods
+    public void raiseSalary(double percent) {
+       if (percent > 0.0) {
+           annualSalary = annualSalary * (1.0 + (percent / 100));
+       }
+    }
+
+    // Getters
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return lastName + ", " + firstName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    // Setters
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
 }
