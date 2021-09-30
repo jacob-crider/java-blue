6ba3d0b6df85bf5825909c1d03f3e55302ce53dd
Jacob Crider <jpc214@gmail.com>
2021-09-30 19:26:04 -0400

    partial samefirstlast

---
+++ module-1/14_Unit_Testing/exercise-student/src/test/java/com/techelevator/SameFirstLastTest.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+import org.junit.Before;
+
+public class SameFirstLastTest {
+
+    private SameFirstLast target;
+
+    @Before
+    public void setup() {
+        target = new SameFirstLast();
+    }
+}
