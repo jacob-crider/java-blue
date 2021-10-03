4aa70692fdda1aa0a6564bf1b3cba408c2fe6587
Brian Lauvray <brian@techelevator.com>
2021-10-03 17:41:07 -0400

    udpated exceptions lecture in student-lecture

--- module-1/16_FileIO_Reading_in/lecture-final/src/main/java/com/techelevator/exceptions/ExceptionsExamples.java
+++ module-1/16_FileIO_Reading_in/lecture-final/src/main/java/com/techelevator/exceptions/ExceptionsExamples.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class ExceptionsExamples {
 
     public void run() {
-        //finallyExample();
+        finallyExample();
         multipleExceptionsExample();
     }
 
--- module-1/16_FileIO_Reading_in/lecture-student/src/main/java/com/techelevator/exceptions/extraexamples/OverdraftException.java
+++ module-1/16_FileIO_Reading_in/lecture-student/src/main/java/com/techelevator/exceptions/extraexamples/OverdraftException.java
@@ -1,4 +1,4 @@
-package com.techelevator.exceptions;
+package com.techelevator.exceptions.extraexamples;
 
 public class OverdraftException extends Exception {
 
