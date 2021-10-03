5984d9f13ff4f39351f2398f522ad3da7577beb5
Brian Lauvray <brian@techelevator.com>
2021-10-03 17:38:12 -0400

    updated exceptions lecture

--- module-1/16_FileIO_Reading_in/lecture-final/src/main/java/com/techelevator/exceptions/ExceptionsExamples.java
+++ module-1/16_FileIO_Reading_in/lecture-final/src/main/java/com/techelevator/exceptions/ExceptionsExamples.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 
 public class ExceptionsExamples {
 
-    public static void main(String[] args) {
+    public void run() {
+        //finallyExample();
+        multipleExceptionsExample();
+    }
+
+    private void finallyExample() {
 
         int[] nums = {1, 2, 3};
         int x = 0;
@@ -15,27 +20,28 @@ public class ExceptionsExamples {
         Try block identifies code that is at risk of throwing an exception
         If the exception occurs the code after the line it was thrown on will be skipped
          */
-        try {
+//        try {
             x = nums[3];
             x = x + 10; // This line only runs if there is no exception on the line above it
-        } catch (ArrayIndexOutOfBoundsException e) {
-            // The catch block only runs if an exception of that type occurs
-            System.out.println("The Array is out of bounds");
-        } finally {
-            // the finally block is always run
-            y = 100;
-        }
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            // The catch block only runs if an exception of that type occurs
+//            System.out.println("The Array is out of bounds");
+//        } finally {
+//            // the finally block is always run
+//            y = 100;
+//        }
 
         System.out.println(x);
         System.out.println(y);
+    }
 
-
+    private void multipleExceptionsExample() {
         String str = "123";
 
         try {
             str.toUpperCase();
             Integer.parseInt(str);
-            str.charAt(10);
+            System.out.println( str.charAt(1) );
         } catch (NullPointerException e) {
             System.out.println("String was null");
         } catch (NumberFormatException e) {
@@ -43,28 +49,13 @@ public class ExceptionsExamples {
         } catch (Exception e) {  // Can use Polymorphism to catch exceptions by their superclass
             System.out.println("Some other error happened");
         }
+    }
 
 
 
-        Scanner input = new Scanner(System.in);
-
-        while(true) {
-            System.out.println("Enter a number: ");
-            String userInput = input.nextLine();
-
-            int number = 0;
-
-            try {
-                System.out.println("Line 1");
-                number = Integer.parseInt(userInput);
-                System.out.println("Line 2");
-            } catch (NumberFormatException e) {
-                System.out.println("Please enter a valid number!");
-                System.out.println("Line 3");
-            }
-            System.out.println("Line 4");
-            System.out.println("You entered " + number);
-        }
+    public static void main(String[] args) {
+        ExceptionsExamples examples = new ExceptionsExamples();
+        examples.run();
     }
 
 }
