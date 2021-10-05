b48027e672a7a8a6b5399faad74b825af62add7f
Brian Lauvray <brian@techelevator.com>
2021-10-05 14:56:53 -0400

    Lecture code and TDD code

--- module-1/00_TDD/lecture-final/src/main/java/com/techelevator/fizzbuzz/FizzBuzz.java
+++ module-1/00_TDD/lecture-final/src/main/java/com/techelevator/fizzbuzz/FizzBuzz.java
@@ -10,8 +10,36 @@ public class FizzBuzz {
     Otherwise return the original number
      */
 
+    private final static int FIZZ_NUMBER = 3;
+    private final static int BUZZ_NUMBER = 5;
+
+    private final static String FIZZBUZZ = "FizzBuzz";
+    private final static String FIZZ = "Fizz";
+    private final static String BUZZ = "Buzz";
+
     public String fizzBuzzIt(int number) {
-        return null;
+
+        String originalNumber = String.valueOf(number);
+        String returnValue = "";
+
+        if (isMultipleOfThree(number)) {
+            returnValue += FIZZ;
+        }
+        if (isMultipleOfFive(number)) {
+            returnValue += BUZZ;
+        }
+
+        return returnValue.length() == 0 ? originalNumber: returnValue;
     }
+
+    private boolean isMultipleOfThree(int number) {
+        return number % FIZZ_NUMBER == 0;
+    }
+
+    private boolean isMultipleOfFive(int number) {
+        return number % BUZZ_NUMBER == 0;
+    }
+
+
 
 }
--- module-1/17_FileIO_Writing/lecture-final/src/main/java/com/techelevator/splitter/SimpleFileSplitter.java
+++ module-1/17_FileIO_Writing/lecture-final/src/main/java/com/techelevator/splitter/SimpleFileSplitter.java
@@ -1,5 +1,8 @@
 package com.techelevator.splitter;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SimpleFileSplitter {
@@ -14,6 +17,42 @@ public class SimpleFileSplitter {
         int lineCount = userInput.nextInt();
         userInput.nextLine();
 
+        File fileObject = new File(path);
+
+        try (Scanner fileScanner = new Scanner(fileObject)) {
+
+            int segmentNumber = 0;
+            while(fileScanner.hasNextLine()) {
+
+                List<String> linesInSegment = new ArrayList<String>();
+
+                int count = lineCount;
+                while(count > 0 && fileScanner.hasNextLine()) {
+                    String lineFromFile = fileScanner.nextLine();
+                    linesInSegment.add( lineFromFile );
+                    count--;
+                }
+
+                String pathToNewSegmentFile = path + "-" + segmentNumber;
+                segmentNumber++;
+
+                try (PrintWriter printWriter = new PrintWriter(pathToNewSegmentFile);
+                     BufferedWriter bufferedWriter = new BufferedWriter(printWriter)) {
+
+                    for (String line : linesInSegment) {
+                        bufferedWriter.write(line);
+                        bufferedWriter.newLine();
+                    }
+                }
+
+
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + fileObject.getAbsolutePath());
+        } catch (IOException e) {
+            System.out.println("Unable to access file: " + e.getMessage());
+        }
 
     }
 }
