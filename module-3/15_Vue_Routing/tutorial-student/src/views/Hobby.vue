e061b98d4cdf5b0b8312cfc5a16b01c805574ceb
Jacob Crider <jpc214@gmail.com>
2021-11-28 19:19:51 -0500

    completed tutorial for M3D15

---
+++ module-3/15_Vue_Routing/tutorial-student/src/views/Hobby.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>My Hobbies</h1>
+      <p>I like to code, play video games, and lift weights.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ module-3/15_Vue_Routing/tutorial-student/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h1>Hi!</h1>
+      <p>My name is Jacob and I am learning how to code in VueJS</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
